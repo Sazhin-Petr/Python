0e61321d1885ed2f7fb5565cfb3c208cfeb4ee3c
Сажин Петр <petiaralich@rambler.ru>
2025-05-21 18:54:28 +0500

    1905

--- main.py
+++ main.py
@@ -5895,11 +5895,26 @@ from zoneinfo import reset_tzpath
 # # print(data[0].keys())
 
 
+import sqlite3
 
+# con = sqlite3.connect('profile.db')
+# cur = con.cursor()
+#
+# cur.execute('''''')
+#
+# con.close()
 
-
-
-
+# with sqlite3.connect('profile.db') as con:
+#     cur = con.cursor()
+#     cur.execute('''CREATE TABLE IF NOT EXISTS users(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     summa REAL,
+#     date BLOB
+#     )''')
+#     cur.execute('DROP TABLE users')
+#
+#
 
 
 
