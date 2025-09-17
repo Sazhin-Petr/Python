c80ed308f998947f1a436203eb7481cff59f32a5
Сажин Петр <petiaralich@rambler.ru>
2025-09-17 19:42:38 +0500

    dz1509

--- js/script.js
+++ js/script.js
@@ -2380,16 +2380,16 @@ $ - конец строки (после последовательности н�
 //   }, 500);
 // }, 1000);
 
-console.log("Клиент: хочу получить список пользователей");
-console.log("...");
+// console.log("Клиент: хочу получить список пользователей");
+// console.log("...");
 
-let promise = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    console.log("Сервер: запрашиваю список пользователей в БД");
-    console.log("...");
-    resolve();
-  }, 1000);
-});
+// let promise = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     console.log("Сервер: запрашиваю список пользователей в БД");
+//     console.log("...");
+//     resolve();
+//   }, 1000);
+// });
 // promise.then(function () {
 //   setTimeout(function () {
 //     console.log("Сервер: трансформирую данные для клиента");
@@ -2397,48 +2397,129 @@ let promise = new Promise(function (resolve, reject) {
 //   }, 500);
 // });
 
-promise
-  .then(function () {
-    return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        let users = [
-          { uid: "id1", name: "Igor" },
-          { uid: "id2", name: "Irina" },
-        ];
-        reject("База данных не смогла получить список ползователей");
-        console.log("БД: формирую список пользователей", users);
-        console.log("...");
-        resolve(users);
-      }, 500);
-    });
-  })
-  .then(function (dbUsers) {
-    return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        console.log("Сервер: трансформирую данные для клиента");
-        console.log("...");
-        let users = dbUsers.map(function (user) {
-          return {
-            id: user.uid,
-            firstName: user.name,
-            timestamp: Date.now(),
-          };
-        });
-        resolve(users);
-      }, 1000);
-    });
-  })
-  .then(function (users) {
-    return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        console.log("Клиент: получил даные и отбражаю их", users);
-        resolve();
-      }, 1000);
-    });
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {
-    console.log("Finally");
-  });
+// promise
+//   .then(function () {
+//     return new Promise(function (resolve, reject) {
+//       setTimeout(function () {
+//         let users = [
+//           { uid: "id1", name: "Igor" },
+//           { uid: "id2", name: "Irina" },
+//         ];
+//         reject("База данных не смогла получить список ползователей");
+//         console.log("БД: формирую список пользователей", users);
+//         console.log("...");
+//         resolve(users);
+//       }, 500);
+//     });
+//   })
+//   .then(function (dbUsers) {
+//     return new Promise(function (resolve, reject) {
+//       setTimeout(function () {
+//         console.log("Сервер: трансформирую данные для клиента");
+//         console.log("...");
+//         let users = dbUsers.map(function (user) {
+//           return {
+//             id: user.uid,
+//             firstName: user.name,
+//             timestamp: Date.now(),
+//           };
+//         });
+//         resolve(users);
+//       }, 1000);
+//     });
+//   })
+//   .then(function (users) {
+//     return new Promise(function (resolve, reject) {
+//       setTimeout(function () {
+//         console.log("Клиент: получил даные и отбражаю их", users);
+//         resolve();
+//       }, 1000);
+//     });
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     console.log("Finally");
+//   });
+
+
+//----------------->
+
+// let test = ms =>{
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(), ms)
+//   })
+// }
+
+// test(1000).then(() => console.log('1000ms'));
+// test(2000).then(() => console.log('2000ms'));
+
+// Promise.all([test(1000), test(2000)]).then(()=>{
+//   console.log('all');
+// });
+
+// Promise.race([test(1000), test(2000)]).then(()=>{
+//   console.log('race');
+// });
+
+
+// let test = ms =>{
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(), ms)
+//   })
+// }
+
+// let p1 = test(1000).then(() => ({name:'1000ms'}));
+// let p2 = test(2000).then(() => ({name:'2000ms'}));
+
+// Promise.all([p1, p2]).then((data)=>{
+//   console.log('all', data);
+// });
+
+// Promise.race([p1, p2]).then((data)=>{
+//   console.log('race', data);
+// });
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos/')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+
+// document.querySelector('#load').addEventListener('click', loadUsers);
+
+// function loadUsers(){
+//   let url = 'https://jsonplaceholder.typicode.com/users/';
+//   fetch(url)
+//   .then(function (response){
+//     return response.json()
+//   }).then(function(data){
+//     let ul = document.querySelector('#list');
+//     let html = data .map(function(item){
+//       return '<li>' + item.id + ' ' + item.name + ' ' + item.email + '</li>'
+//     })
+//     ul.insertAdjacentHTML('afterbegin', html.join(' '))
+//   })
+// }
+
+
+// document.querySelector('#load').addEventListener('click', loadUsers);
+
+// async function loadUsers(){
+//   let url = 'https://jsonplaceholder.typicode.com//';  // todos
+//   let response = await fetch(url);
+//   let data = await response.json();
+//   let html = data .map(function(item){
+//       return '<li>' + item.id + ' ' + item.name + ' ' + item.email + '</li>'
+//     })
+//     document.querySelector('#list').insertAdjacentHTML('afterbegin', html.join(' '))
+
+// }
+
+
+//   Game-------->\
+
+let box = document.querySelector('#box');
+console.log(box.getBoundingClientRect());
+
